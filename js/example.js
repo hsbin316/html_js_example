bf59a1dc494a248c8e2c6c1245eccabe51a19768
허성빈 <hsbin316@gmail.com>
2022-12-08 15:01:42 +0900

    onclick이벤트와 prompt를 이용한 이름 바꾸기

---
+++ js/example.js
@@ -0,0 +1,26 @@
+const nameH1Element = document.querySelector('h1.inline');
+const connectNameElement = document.querySelector('span.red');
+// const inputModalElement = document.querySelector('#inputModal');
+
+const setUserName = (name) => {
+  nameH1Element.textContent = name;
+  connectNameElement.textContent = name;
+};
+
+const localName = localStorage.getItem('name');
+if (localName) setUserName(localName);
+
+nameH1Element.onclick = () => {
+  // inputModalElement.showModal();
+  const inputName = prompt('이름을 입력해 주세요.');
+  if (inputName) {
+    localStorage.setItem('name', inputName);
+
+    setUserName(inputName);
+  } else {
+    alert('이름이 입력되지 않았습니다.');
+  }
+};
+// inputModalElement.onclick = (event) => {
+//   if (event.target.nodeName === 'DIALOG') inputModalElement.closest();
+// };
